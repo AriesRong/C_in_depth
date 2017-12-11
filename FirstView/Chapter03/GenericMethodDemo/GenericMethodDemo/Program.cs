b1ef3b710c610ea5db7d093be01f1c68be408dd2
AriesRong <1121069591@qq.com>
2017-12-11 15:35:43 +0800

    Chapter03_FirstView

---
+++ FirstView/Chapter03/GenericMethodDemo/GenericMethodDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace GenericMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = MakeList<string>("Line 1", "Line 2");
+            foreach(string x in list)
+            {
+                Console.WriteLine(x);
+            }
+            List<int> number = MakeList<int>(5, 3);
+            foreach(int num in number)
+            {
+                Console.WriteLine(num);
+            }
+            Console.Read();
+        }
+
+        static List<T> MakeList<T>(T first,T second)
+        {
+            List<T> list = new List<T>();
+            list.Add(first);
+            list.Add(second);
+            return list;
+        }
+    }
+}
---
+++ FirstView/Chapter03/ListCovertAll/ListCovertAll/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ListCovertAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = new List<int>();
+            integers.Add(1);
+            integers.Add(2);
+            integers.Add(3);
+            integers.Add(4);
+
+            Converter<int, double> coverter = TakeSquareRoot;
+            List<double> doubles = integers.ConvertAll<double>(coverter);
+
+            foreach(double d in doubles)
+            {
+                Console.WriteLine(d);
+            }
+            Console.Read();
+        }
+        static double TakeSquareRoot(int x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
+}
